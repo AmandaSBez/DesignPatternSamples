44b3932c4400a3b36655af2fc43bfd2a1417dc52
Amanda Bezerra <amanda.sousabez@gmail.com>
2021-01-09 17:25:36 -0200

    Primeiro Commit

---
+++ src/Application/Repository/IDetranVerificadorDebitosRepository.cs
@@ -0,0 +1,12 @@
+﻿using DesignPatternSamples.Application.DTO;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesignPatternSamples.Application.Repository
+{
+    public interface IDetranVerificadorDebitosRepository
+    {
+        //A interface diz: voce precisa implementar o metodo ConsultarDebitos recebendo um veiculo e voce vai me devolver uma lista de debitos de veiculo
+        Task<IEnumerable<DebitoVeiculo>> ConsultarDebitos(Veiculo veiculo);
+    }
+}
---
+++ src/Infra.Repository.Detran/DetranVerificadorDebitosRepositoryCrawlerBase.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatternSamples.Application.DTO;
+using DesignPatternSamples.Application.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DesignPatternSamples.Infra.Repository.Detran
+{
+    public abstract class DetranVerificadorDebitosRepositoryCrawlerBase : IDetranVerificadorDebitosRepository
+    {
+        // Para ele fazer esse metodo para a interface a classe abstrata pede para realizar esses dois metodos RealizarAcesso e PadronizarResultado
+        public async Task<IEnumerable<DebitoVeiculo>> ConsultarDebitos(Veiculo veiculo)
+        {
+            var html = await RealizarAcesso(veiculo);
+            return await PadronizarResultado(html);
+        }
+
+        protected abstract Task<string> RealizarAcesso(Veiculo veiculo);
+        protected abstract Task<IEnumerable<DebitoVeiculo>> PadronizarResultado(string html);
+    }
+}
